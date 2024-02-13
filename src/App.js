a4fd4948c90f5355afe85889cfd9facfe3bafe33
Leon Gaban <leongaban@gmail.com>
2024-02-12 20:54:48 -0600

    adding Clarifai & fixed process undefined error

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React from 'react'
+// import Clarifai from 'clarifai'
+
 import Navigation from './components/Navigation/Navigation'
 import Logo from './components/Logo/Logo'
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm'
 import './App.css'
+
+// https://github.com/Clarifai/clarifai-nodejs-grpc
+// https://clarifai.com/clarifai/main/models/face-detection
+// https://github.com/aneagoie/face-recognition-brain/blob/master/src/App.js
+
+// Set NodeJS default version: https://www.bairesdev.com/blog/change-node-version/
+
+//You must add your own API key here from Clarifai.
+// const app = new Clarifai.App({
+//   apiKey: 'YOUR API KEY HERE',
+// })
 
 function App() {
   return (
--- src/index.js
+++ src/index.js
@@ -1,27 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 
-import App from './App';
+import App from './App'
 import videoBg from './assets/face-background.mp4'
-import './index.css';
-import reportWebVitals from './reportWebVitals';
+import './index.css'
+import reportWebVitals from './reportWebVitals'
+
+console.log('window', window)
+
+let process = {}
+
+console.log('process', process)
+// if (process.env.NODE_ENV !== 'production') {
+// }
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-<div>
-  <App />
-  <div className='hero'>
-    <div className='content'>
-      <h1>FACE RECOGNITION BRAIN<em>TM</em></h1>
-    </div>
-    <video src={videoBg} autoPlay loop muted/>
-    {/* <video autoPlay loop muted plays-inline="true" className="back-video">
+  <div>
+    <App />
+    <div className="hero">
+      <div className="content">
+        <h1>
+          FACE RECOGNITION BRAIN<em>TM</em>
+        </h1>
+      </div>
+      <video src={videoBg} autoPlay loop muted />
+      {/* <video autoPlay loop muted plays-inline="true" className="back-video">
       <source src="assets/face-background.mp4" type="video/mp4"/>
     </video> */}
+    </div>
   </div>
-</div>
-);
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
